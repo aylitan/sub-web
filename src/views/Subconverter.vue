20b7c0c9a26b41f2d3a0abf0d82d35be8b63df68
aylitan <46384604+aylitan@users.noreply.github.com>
2021-01-31 21:32:30 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -268,7 +268,7 @@ export default {
         },
         customBackend: {
           "localhost:25500 本地版": "http://localhost:25500/sub?",
-          "aylitan.tk (自动编译最新版本后端-测试）": "https://aylitan.tk/sub?",
+          "sub.aylitan.tk (自动编译最新版本后端-测试）": "https://sub.aylitan.tk/sub?",
           "subcon.dlj.tf(subconverter作者提供-稳定)":
             "https://subcon.dlj.tf/sub?",
           "api.dler.io(sub作者&lhie1提供-稳定)": "https://api.dler.io/sub?",
@@ -276,7 +276,7 @@ export default {
         },
         backendOptions: [
           { value: "http://localhost:25500/sub?" },
-          { value: "https://aylitan.tk/sub?" },
+          { value: "https://sub.aylitan.tk/sub?" },
           { value: "https://subcon.dlj.tf/sub?" },
           { value: "https://api.dler.io/sub?" },
           { value: "https://api.wcc.best/sub?" },
@@ -595,7 +595,7 @@ export default {
   },
   mounted() {
     this.form.clientType = "clash&new_name=true";
-    this.form.customBackend = "https://aylitan.tk/sub?";
+    this.form.customBackend = "https://sub.aylitan.tk/sub?";
     this.form.remoteConfig = "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online.ini";
     this.getBackendVersion();
   },
